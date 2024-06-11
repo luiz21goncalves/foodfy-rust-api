7496a8b8dd37115dfc611311afb17efa58565205
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-06-11 20:55:26 -0300

    feat: response a json value

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
+use serde_json::json;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello World" }));
+    let app = Router::new().route(
+        "/",
+        get(|| async { Json(json!({"message":"Hello World"})) }),
+    );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3333").await.unwrap();
     axum::serve(listener, app).await.unwrap()
