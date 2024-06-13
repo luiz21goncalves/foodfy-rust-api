319fdb27c8767217d1d9792c35207d2fe5ae16b9
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-06-13 15:05:54 -0300

    feat: show database status

--- src/controllers/status.rs
+++ src/controllers/status.rs
@@ -1,17 +1,68 @@
-use axum::Json;
+use axum::{extract::State, Json};
 use chrono::{DateTime, Utc};
 use serde::Serialize;
+use sqlx::{Pool, Postgres};
+
+#[derive(Serialize)]
+pub struct Database {
+    version: String,
+    max_connections: i32,
+    opened_connections: i32,
+}
 
 #[derive(Serialize)]
 pub struct Status {
     version: String,
-    date: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
+    database: Database,
+}
+struct Count {
+    count: Option<i32>,
+}
+struct PostgresVersion {
+    server_version: Option<String>,
 }
 
-pub async fn api_status() -> Json<Status> {
+struct MaxConnections {
+    max_connections: Option<String>,
+}
+
+pub async fn api_status(State(pool): State<Pool<Postgres>>) -> Json<Status> {
+    let postgres_version = sqlx::query_as!(PostgresVersion, "SHOW server_version")
+        .fetch_one(&pool)
+        .await
+        .unwrap()
+        .server_version
+        .unwrap();
+
+    let opened_connections = sqlx::query_as!(
+        Count,
+        "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+        "foodfy",
+    )
+    .fetch_one(&pool)
+    .await
+    .unwrap()
+    .count
+    .unwrap();
+
+    let max_connections = sqlx::query_as!(MaxConnections, "SHOW max_connections;")
+        .fetch_one(&pool)
+        .await
+        .unwrap()
+        .max_connections
+        .unwrap()
+        .parse::<i32>()
+        .unwrap();
+
     let status = Status {
         version: env!("CARGO_PKG_VERSION").to_string(),
-        date: chrono::Utc::now(),
+        updated_at: chrono::Utc::now(),
+        database: Database {
+            version: postgres_version,
+            max_connections,
+            opened_connections,
+        },
     };
 
     Json(status)
