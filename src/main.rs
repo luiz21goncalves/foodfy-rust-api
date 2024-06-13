21c8800023024d1146cccb9c73e93253ac300f91
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-06-13 14:49:25 -0300

    chore: add postgres connection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{routing::get, Router};
+use sqlx::postgres::PgPoolOptions;
 use tokio::net::TcpListener;
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
@@ -15,9 +16,16 @@ async fn main() {
 
     let v1_routes = Router::new().nest("/status", status_routes);
 
+    let database_url = std::env::var("DATABASE_URL").ok().unwrap_or(String::from(
+        "postgres://docker:docker@localhost:5432/foodfy",
+    ));
+
+    let pool = PgPoolOptions::new().connect(&database_url).await.unwrap();
+
     let app = Router::new()
         .nest("/v1", v1_routes)
-        .layer(TraceLayer::new_for_http());
+        .layer(TraceLayer::new_for_http())
+        .with_state(pool);
 
     let port = std::env::var("PORT").ok().unwrap_or(String::from("3333"));
 
